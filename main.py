dd084c0ce2209b527a5e3a22d00016cf2da20a25
Kano <dpkano@gmail.com>
2016-12-07 14:27:15 -0600

    switch delete to true

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route('/api/status')
 def getStatus():
     """return status"""
-    tel = {'delete': False, 'fetch': True, 'insert': True, 'list': True}
+    tel = {'delete': True, 'fetch': True, 'insert': True, 'list': True}
     return jsonify(tel), 200
 
 @app.route('/api/capitals', methods=['GET'])
