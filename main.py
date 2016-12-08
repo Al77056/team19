0296ec387c87cbbeb145943c98c771c0526ebbbb
Albert Lu <zlu6@slb.com>
2016-12-08 12:28:09 -0600

    enable storage and pubsub

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route('/api/status')
 def getStatus():
     """return status"""
-    tel = {'delete': True, 'fetch': True, 'insert': True, 'list': True, "query": False, "search": False, "pubsub": False, "storage": False}
+    tel = {'delete': True, 'fetch': True, 'insert': True, 'list': True, "query": False, "search": False, "pubsub": True, "storage": True}
     return jsonify(tel), 200
 
 @app.route('/api/capitals', methods=['GET'])
